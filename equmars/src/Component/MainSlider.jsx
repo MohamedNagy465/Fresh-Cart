04b2eb9a5ec12bd7e40f4a435255d99c336b6725
MohamedNagy <ms8912568@gmail.com>
2025-07-31 04:42:44 +0300

    Change main slider

--- equmars/src/Component/MainSlider.jsx
+++ equmars/src/Component/MainSlider.jsx
@@ -1,35 +1,59 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css'; 
-import 'slick-carousel/slick/slick-theme.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function MainSlider() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-  };
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false, // لو عايز تشيل الأسهم
+};
 
+export default function HomeSliderSection() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 py-6">
-      {/* Main Slider */}
-      <div className="md:col-span-2 h-[400px]">
+      <div className="md:col-span-2 mb-2 h-[400px]">
         <Slider {...settings}>
-          <img src="/slider1.jpeg" className="w-full h-[420px] rounded-lg object-cover" alt="Slide 1" />
-          <img src="/slider2.jpg" className="w-full h-[420px] rounded-lg object-cover" alt="Slide 2" />
-          <img src="/slider3.jpg" className="w-full h-[420px] rounded-lg object-cover" alt="Slide 3" />
+          <div className="h-[420px] rounded-lg overflow-hidden">
+            <img
+              src="/slider1.jpeg"
+              className="w-full h-full object-cover transition-all duration-300"
+              alt="Slide 1"
+            />
+          </div>
+          <div className="h-[420px] rounded-lg overflow-hidden">
+            <img
+              src="/slider2.jpg"
+              className="w-full h-full object-cover transition-all duration-300"
+              alt="Slide 2"
+            />
+          </div>
+          <div className="h-[420px] rounded-lg overflow-hidden">
+            <img
+              src="/slider3.jpg"
+              className="w-full h-full object-cover transition-all duration-300"
+              alt="Slide 3"
+            />
+          </div>
         </Slider>
       </div>
 
       {/* Side Images */}
-      <div className="flex flex-col gap-4 h-[400px]">
-        <img src="/slider4.jpg" className="w-full h-1/2 rounded-lg object-cover" alt="Side 1" />
-        <img src="/slider5.jpg" className="w-full h-1/2 rounded-lg object-cover" alt="Side 2" />
+      <div className="flex flex-col gap-5 h-[400px]">
+        <img
+          src="/slider4.jpg"
+          className="w-full h-1/2 rounded-lg object-cover"
+          alt="Side 1"
+        />
+        <img
+          src="/slider5.jpg"
+          className="w-full h-1/2 rounded-lg object-cover"
+          alt="Side 2"
+        />
       </div>
     </div>
   );
